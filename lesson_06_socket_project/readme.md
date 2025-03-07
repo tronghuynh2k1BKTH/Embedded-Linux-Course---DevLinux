c0b71b8a843c730458639514449ca111e061f74e
tronghuynh2k1BKTH <huynhtrong2302khanhbao@gmail.com>
2025-03-07 16:23:55 +0000

    update mini project

--- lesson_06_socket_project/readme.md
+++ lesson_06_socket_project/readme.md
@@ -1,3 +1,20 @@
+
+concept:
+    ./temp 4000 -> create a thread and create socket on thread
+-main:
+    - handle argument port -> create thread
+	- create socket, bind
+	- while(1)
+		- check user type:
+            - help: show all cmd definded
+            - myip: show ip of thread
+            - list: show all socket connected
+            - connect <ip> <port>: to connect to this socket
+            - terminate <ip> <port>: to terminate this socket
+            - send <ip> <port> <message>: to send a message to this socket
+            - exit: end all socket in this process (temp.c)
+
+-----------------
 HUONG TIEP CAN
 
 1. create socket TCP & server
@@ -7,12 +24,23 @@ HUONG TIEP CAN
 - Dùng **accept()** để chấp nhận kết nối mới.
 
 
+----------------- common cmds
+check process has port 4000
+    sudo netstat -tulnp | grep :4000
 
------------------
-CAU HOI THAO LUAN
+check & stop process with PID
+    ps -aux | grep 46051
+    kill -9 46051
+
+----------------- QnA
 
 Không cần khai báo trước 3 socket cố định!
+    - Chỉ cần **1 socket “listen”** để chờ kết nối.
+    - Khi có client kết nối đến, **tạo thêm socket mới động (dynamically)** để xử lý từng kết nối.
 
-- Chỉ cần **1 socket “listen”** để chờ kết nối.
-- Khi có client kết nối đến, **tạo thêm socket mới động (dynamically)** để xử lý từng kết nối.
+why has a different for 1 thread has 2 port
+    - side on main thread: myport is 4000
+    - side on connection: list is another port is 51464
+    -----> no no no
 
+open many socket to listen on 1 process?
--- lesson_06_socket_project/temp.c
+++ lesson_06_socket_project/temp.c
@@ -3,120 +3,223 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <ifaddrs.h>
-#include <errno.h>
-#include <netdb.h>
+#include <pthread.h>
 
-#define PORT 8080
-#define MAX_CONNECTIONS 10
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
 
-void print_help() {
-    printf("Available commands:\n");
-    printf("help - Show this help message\n");
-    printf("myip - Show the IP address of this machine\n");
-    printf("myport - Show the port number this server is listening on\n");
-    printf("connect <ip> <port> - Connect to a server\n");
-    printf("list - Show all connections\n");
-    printf("terminate <ip> <port> - Terminate a connection\n");
-    printf("send <ip> <port> \"message\" - Send a message to a connection\n");
-    printf("exit - Exit the application\n");
-}
+typedef struct {
+    int socket_fd;
+    struct sockaddr_in address;
+} Connection;
 
-void show_my_ip() {
-    struct ifaddrs *ifaddr, *ifa;
-    char host[NI_MAXHOST];
+Connection client_list[MAX_CLIENTS];
+int client_count = 0;
+int server_socket;
 
-    if (getifaddrs(&ifaddr) == -1) {
-        perror("getifaddrs");
-        exit(EXIT_FAILURE);
+// Function to start the server
+void start_server(int port) {
+    struct sockaddr_in server_addr;
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(1);
     }
 
-    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
-        if (ifa->ifa_addr == NULL)
-            continue;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
 
-        int family = ifa->ifa_addr->sa_family;
-        if (family == AF_INET) {
-            int s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in),
-                                host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-            if (s != 0) {
-                printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                exit(EXIT_FAILURE);
-            }
-            printf("Interface: %s\tAddress: %s\n", ifa->ifa_name, host);
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Bind failed");
+        exit(1);
+    }
+
+    if (listen(server_socket, MAX_CLIENTS) == -1) {
+        perror("Listen failed");
+        exit(1);
+    }
+
+    printf("Server started on port %d. Waiting for connections...\n", port);
+}
+
+// Function to connect to another socket
+void connect_to_socket(char *ip, int port) {
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        return;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        close(sockfd);
+        return;
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        close(sockfd);
+        return;
+    }
+
+    if (client_count < MAX_CLIENTS) {
+        client_list[client_count].socket_fd = sockfd;
+        client_list[client_count].address = server_addr;
+        client_count++;
+        printf("Connected to %s:%d successfully.\n", ip, port);
+    } else {
+        printf("Client list full. Cannot store more connections.\n");
+        close(sockfd);
+    }
+}
+
+// Function to send a message to a connected socket
+void send_message(char *ip, int port, char *message) {
+    for (int i = 0; i < client_count; i++) {
+        if (ntohs(client_list[i].address.sin_port) == port &&
+            strcmp(inet_ntoa(client_list[i].address.sin_addr), ip) == 0) {
+            send(client_list[i].socket_fd, message, strlen(message), 0);
+            printf("Message sent to %s:%d\n", ip, port);
+            return;
         }
     }
-
-    freeifaddrs(ifaddr);
+    printf("Error: No connection found for %s:%d\n", ip, port);
 }
 
-void show_my_port() {
-    printf("Listening on port: %d\n", PORT);
+// Function to terminate a connection
+void terminate_connection(char *ip, int port) {
+    for (int i = 0; i < client_count; i++) {
+        if (ntohs(client_list[i].address.sin_port) == port &&
+            strcmp(inet_ntoa(client_list[i].address.sin_addr), ip) == 0) {
+            close(client_list[i].socket_fd);
+            printf("Connection to %s:%d terminated.\n", ip, port);
+            for (int j = i; j < client_count - 1; j++) {
+                client_list[j] = client_list[j + 1];
+            }
+            client_count--;
+            return;
+        }
+    }
+    printf("Error: No connection found for %s:%d\n", ip, port);
 }
 
-int main() {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+// Function to list all active connections
+void list_connections() {
+    printf("Active connections:\n");
+    for (int i = 0; i < client_count; i++) {
+        printf("Socket FD: %d | IP: %s | Port: %d\n",
+               client_list[i].socket_fd,
+               inet_ntoa(client_list[i].address.sin_addr),
+               ntohs(client_list[i].address.sin_port));
+    }
+}
+
+// Function to handle incoming connections
+void *handle_clients(void *arg) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+
+    while (1) {
+        int new_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+        if (new_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        if (client_count < MAX_CLIENTS) {
+            client_list[client_count].socket_fd = new_socket;
+            client_list[client_count].address = client_addr;
+            client_count++;
+        } else {
+            printf("Max clients reached. Connection refused.\n");
+            close(new_socket);
+        }
+
+        memset(buffer, 0, BUFFER_SIZE);
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("Received: %s\n", buffer);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    pthread_t client_thread;
+    start_server(port);
+    pthread_create(&client_thread, NULL, handle_clients, NULL);
+
     char command[256];
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, MAX_CONNECTIONS) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server is listening on port %d\n", PORT);
-
     while (1) {
         printf("Enter command: ");
         fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = 0; // Remove newline character
+        command[strcspn(command, "\n")] = 0; // Remove newline
 
-        if (strcmp(command, "help") == 0) {
-            print_help();
-        } else if (strcmp(command, "myip") == 0) {
-            show_my_ip();
-        } else if (strcmp(command, "myport") == 0) {
-            show_my_port();
-        } else if (strncmp(command, "connect", 7) == 0) {
-            // Implement connect functionality
-        } else if (strcmp(command, "list") == 0) {
-            // Implement list functionality
-        } else if (strncmp(command, "terminate", 9) == 0) {
-            // Implement terminate functionality
+        if (strncmp(command, "connect", 7) == 0) {
+            char ip[50];
+            int port;
+            if (sscanf(command, "connect %s %d", ip, &port) == 2) {
+                connect_to_socket(ip, port);
+            } else {
+                printf("Usage: connect <ip> <port>\n");
+            }
         } else if (strncmp(command, "send", 4) == 0) {
-            // Implement send functionality
+            char ip[50], message[BUFFER_SIZE];
+            int port;
+            if (sscanf(command, "send %s %d %[^\n]", ip, &port, message) == 3) {
+                send_message(ip, port, message);
+            } else {
+                printf("Usage: send <ip> <port> <message>\n");
+            }
+        } else if (strncmp(command, "terminate", 9) == 0) {
+            char ip[50];
+            int port;
+            if (sscanf(command, "terminate %s %d", ip, &port) == 2) {
+                terminate_connection(ip, port);
+            } else {
+                printf("Usage: terminate <ip> <port>\n");
+            }
+        } else if (strcmp(command, "list") == 0) {
+            list_connections();
         } else if (strcmp(command, "exit") == 0) {
+            for (int i = 0; i < client_count; i++) {
+                close(client_list[i].socket_fd);
+            }
+            close(server_socket);
+            printf("Server shutting down.\n");
             break;
-        } else {
-            printf("Unknown command. Type 'help' for a list of commands.\n");
+        } else if (strcmp(command, "myip") == 0) {
+            system("hostname -I");
+        } else if (strcmp(command, "myport") == 0) {
+            printf("Port: %d\n", port);
+        } else if (strcmp(command, "help") == 0) {
+            printf("\nCommands:\n\n");
+            printf("connect <ip> <port>\n");
+            printf("send <ip> <port> <message>\n");
+            printf("terminate <ip> <port>\n");
+            printf("list: show all connections\n");
+            printf("exit: to exit all sockets\n");
+            printf("myip: show current ip process\n");
+            printf("myport: show current port process\n");
+            printf("help: show all commands for app\n\n");
+        }
+        else {
+            printf("Unknown command.\n");
         }
     }
 
-    close(server_fd);
     return 0;
 }
